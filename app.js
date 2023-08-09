def66f22c67f7f21f5371860dc3ad2781d857a2d
Nazishkhan123 <132814225+Nazishkhan123@users.noreply.github.com>
2023-08-09 05:30:15 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,105 @@
+function changeImage(){
+    var image = document.getElementById('img')
+    image.src = 'img2.jpeg'
+}
+
+function changeImageBefore(){
+    var image = document.getElementById('img')
+    image.src = 'img3.jpeg'
+}
+function changeImage1(){
+    var image = document.getElementById('imgs')
+    image.src = 'img4.png'
+}
+
+function changeImageBefore1(){
+    var image = document.getElementById('imgs')
+    image.src = 'img6.png'
+}
+function changeImage2(){
+    var image = document.getElementById('imges')
+    image.src = 'img7.png'
+}
+
+function changeImageBefore2(){
+    var image = document.getElementById('imges')
+    image.src = 'img8.png'
+}
+function changeImage3(){
+    var image = document.getElementById('imgh')
+    image.src = 'img9.png'
+}
+
+function changeImageBefore3(){
+    var image = document.getElementById('imgh')
+    image.src = 'img12.jpeg'
+}
+
+// gggggggggggg
+function changeImage4(){
+    var image = document.getElementById('img1')
+    image.src = 'img10.png'
+}
+
+function changeImageBefore4(){
+    var image = document.getElementById('img1')
+    image.src = 'img1.jpeg'
+}
+function changeImage5(){
+    var image = document.getElementById('imgs2')
+    image.src = 'img14.jpeg'
+}
+
+function changeImageBefore5(){
+    var image = document.getElementById('imgs2')
+    image.src = 'img6.png'
+}
+function changeImage6(){
+    var image = document.getElementById('imges3')
+    image.src = 'img15.jpeg'
+}
+
+function changeImageBefore6(){
+    var image = document.getElementById('imges3')
+    image.src = 'img8.png'
+}
+function changeImage7(){
+    var image = document.getElementById('imgh4')
+    image.src = 'img9.png'
+}
+
+function changeImageBefore7(){
+    var image = document.getElementById('imgh4')
+    image.src = 'img12.jpeg'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function readMore(){
+    var pera = document.getElementById('pera')
+    pera.innerText = "Some quick example text to build on the card title and make up the bulk of the card's content on the card title and make up the bulk of the card's content."
+}
+function readMores(){
+    var peras = document.getElementById('peras')
+    peras.innerText = "Some quick example text to build on the card title and make up the bulk of the card's content on the card title and make up the bulk of the card's content."
+}
+function readMoress(){
+    var peraes = document.getElementById('peraes')
+    peraes.innerText = "Some quick example text to build on the card title and make up the bulk of the card's content on the card title and make up the bulk of the card's content."
+}
+
+
+
